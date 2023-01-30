2984be4488d8ecea674b76f61a235193d1367121
itscyanne <cyannejustinl.vega12@gmail.com>
2023-01-30 17:12:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,38 +15,11 @@ async def on_ready():
     print(f'Logged in as {bot.user} (ID: {bot.user.id})')
     print('------')
 
-    scheduler = AsyncIOScheduler()
-
-    # sends "Your Message" at 12PM and 18PM (Local Time)
-    scheduler.add_job(sayGoodNight, CronTrigger(hour="23", minute="00", second="00"))
-    scheduler.add_job(sayGoodMorning, CronTrigger(hour="8", minute="00", second="00"))
-
-    # starting the scheduler
-    scheduler.start()
-
-
-async def sayGoodNight():
-    await bot.wait_until_ready()
-    c = bot.get_channel(1015869733809176638)
-    await c.send("good night, babe!")
-
-
-async def sayGoodMorning():
-    await bot.wait_until_ready()
-    c = bot.get_channel(1015869733809176638)
-    await c.send("good morning, babe!")
-
 
 @bot.command()
 async def calc(ctx, operation: str):
     await ctx.send(eval(operation))
 
-
-@bot.command()
-async def love(ctx, times: int, name=''):
-    c = bot.get_channel(1015869733809176638)
-    for i in range(times):
-        await c.send("i love you, babe! " + name)
 
 
 @bot.command()
